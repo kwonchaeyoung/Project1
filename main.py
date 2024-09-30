476c1b31de63aba6cbc52442bfde417bad63fc26
KwonChaeYoung <k.chaey01@gmail.com>
2024-09-30 14:46:37 +0900

    커밋 2
    빼기 기능 구현~~~
    ~~~~~

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 '''
+일자 : 2024년 09월 30일 월요일
 교재 : 깃특강2-깃 버전 관리 기본(Ver1.3.1)p.pdf p.36
-
 '''
 
 # 함수 선언부
 def add_func(n1, n2):
     return n1 + n2
+
+def sub_func(n1, n2):
+    return n1 - n2
 
 # 전역 변수부
 num1, num2, res = 100, 200, 0
@@ -13,3 +16,6 @@ num1, num2, res = 100, 200, 0
 # 메인 코드부
 res = add_func(num1, num2)
 print(num1, '+', num2, '=', res)
+
+res = sub_func(num1, num2)
+print(num1, '-', num2, '=', res)
